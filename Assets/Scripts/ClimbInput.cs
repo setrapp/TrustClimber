635cd6e18b8885d46713baa92c99461190169829
Sam Trapp <set9069@rit.edu>
2014-10-08 01:38:09 -0400

    Climbing to letter based handholds.

--- Assets/Scripts/ClimbInput.cs
+++ Assets/Scripts/ClimbInput.cs
@@ -6,6 +6,7 @@ public class ClimbInput : MonoBehaviour {
 	public GameObject rightHand;
 	private bool movingLeftHand = true;
 	private float bodyBelowHands;
+	public float maxArmDistance;
 
 	void Awake()
 	{
@@ -14,15 +15,39 @@ public class ClimbInput : MonoBehaviour {
 
 	void Update()
 	{
-		if (Input.GetKeyDown(KeyCode.Space))
+		Handhold nextHandhold = null;
+		Vector3 handPos = leftHand.transform.position;
+		if (!movingLeftHand)
+		{
+			handPos = rightHand.transform.position;
+		}
+
+		if (Input.GetKeyDown("w"))
+		{
+			nextHandhold = HandholdManager.Instance.NearestHandhold(Handhold.ButtonType.W, handPos, transform.position.y);
+		}
+		else if (Input.GetKeyDown("a"))
+		{
+			nextHandhold = HandholdManager.Instance.NearestHandhold(Handhold.ButtonType.A, handPos, transform.position.y);
+		}
+		else if (Input.GetKeyDown("s"))
+		{
+			nextHandhold = HandholdManager.Instance.NearestHandhold(Handhold.ButtonType.S, handPos, transform.position.y);
+		}
+		else if (Input.GetKeyDown("d"))
+		{
+			nextHandhold = HandholdManager.Instance.NearestHandhold(Handhold.ButtonType.D, handPos, transform.position.y);
+		}
+
+		if (nextHandhold != null && (nextHandhold.transform.position - transform.position).sqrMagnitude <= Mathf.Pow(maxArmDistance, 2))
 		{
 			if (movingLeftHand)
 			{
-				leftHand.transform.position += new Vector3(0, 1, 0);
+				leftHand.transform.position = nextHandhold.transform.position;
 			}
 			else
 			{
-				rightHand.transform.position += new Vector3(0, 1, 0);
+				rightHand.transform.position = nextHandhold.transform.position;
 			}
 
 			movingLeftHand = !movingLeftHand;
---
+++ Assets/Scripts/HandholdManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandholdManager : MonoBehaviour {
+	private static HandholdManager instance = null;
+	public static HandholdManager Instance
+	{
+		get
+		{
+			if (instance == null)
+			{
+				instance = GameObject.FindGameObjectWithTag("Globals").GetComponent<HandholdManager>();
+			}
+			return instance;
+		}
+	}
+
+	public Handhold NearestHandhold(Handhold.ButtonType buttonType, Vector3 fromPosition, float aboveY)
+	{
+		GameObject[] handholds = GameObject.FindGameObjectsWithTag("Handhold");
+		Handhold nearHandhold = null;
+		float minSqrDist = 0;
+		for (int i = 0; i < handholds.Length; i++)
+		{
+			Handhold handhold = handholds[i].GetComponent<Handhold>();
+			float sqrDist = (handholds[i].transform.position - fromPosition).sqrMagnitude;
+			if ((handhold != null && handhold.buttonType == buttonType && handhold.transform.position.y > aboveY) && (nearHandhold == null || sqrDist < minSqrDist))
+			{
+				nearHandhold = handhold;
+				minSqrDist = sqrDist;
+			}
+		}
+
+		return nearHandhold;
+	}
+}
