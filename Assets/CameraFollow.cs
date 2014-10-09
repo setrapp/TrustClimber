a0dd464d5c55f0de12fc390a029730e507c54f41
unknown <aa6096@IGMGRAD-14.main.ad.rit.edu>
2014-10-09 14:38:21 -0400

    Added temp camera movement. Need to smoothen lerp.

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform climber1;
+    public Transform climber2;
+    public float smoothness;
+    public Vector3 targetPosition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        targetPosition = new Vector3((climber1.position.x + climber2.position.x) / 2, (climber1.position.y + climber2.position.y) / 2, -10);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, 0.5f);
+	}
+}
