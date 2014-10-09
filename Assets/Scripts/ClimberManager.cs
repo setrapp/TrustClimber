3c4ac71f213a6326795b2b7ee92b5c32966d3df6
Sam Trapp <set9069@rit.edu>
2014-10-09 16:26:59 -0400

    Added basic fail state of falling off camera.

--- Assets/Scripts/ClimberManager.cs
+++ Assets/Scripts/ClimberManager.cs
@@ -43,7 +43,13 @@ public class ClimberManager : MonoBehaviour {
 
 	void Update()
 	{
-
+		Vector3 climb1ViewportPos = Camera.main.WorldToViewportPoint(climber1.transform.position);
+		Vector3 climb2ViewportPos = Camera.main.WorldToViewportPoint(climber2.transform.position);
+		if (climb1ViewportPos.x < 0 || climb1ViewportPos.x > 1 || climb1ViewportPos.y < 0 || climb1ViewportPos.y > 1 || 
+			climb2ViewportPos.x < 0 || climb2ViewportPos.x > 1 || climb2ViewportPos.y < 0 || climb2ViewportPos.y > 1)
+		{
+			Application.LoadLevel(Application.loadedLevelName);
+		}
 	}
 
 	void OnDrawGizmos()
